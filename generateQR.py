6d666af6c6c4adffb0d9d0734a64111f7c6695e9
edleijnse <ed@leijnse.info>
2024-04-20 11:56:45 +0200

    QR tests added

--- generateQR.py
+++ generateQR.py
@@ -14,10 +14,10 @@ def generate_qrcode():
     url = pyqrcode.create(s)
 
     # Create and save the svg file naming "myqr.svg"
-    url.svg("myqr.svg", scale=8)
+    url.svg(s+".svg", scale=8)
 
     # Create and save the png file naming "myqr.png"
-    url.png('myqr.png', scale=6)
+    url.png(s+'.png', scale=6)
 
 
 # Creating tkinter window
