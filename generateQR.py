ec1fc6edbee1970fdd3684c933834de3e401b637
edleijnse <ed@leijnse.info>
2024-04-20 13:45:08 +0200

    Display of QR Code

--- generateQR.py
+++ generateQR.py
@@ -2,23 +2,32 @@ import pyqrcode
 import png
 from pyqrcode import QRCode
 from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import os
 
 
 # Function to generate QR Code
+# Function to generate QR Code
 def generate_qrcode():
-
     # String which represents the QR code
     s = entry_text.get()
 
     # Generate QR code
     url = pyqrcode.create(s)
 
-    # Create and save the svg file naming "myqr.svg"
-    url.svg(s+".svg", scale=8)
-
     # Create and save the png file naming "myqr.png"
-    url.png(s+'.png', scale=6)
-    url.show()
+    png_file = s + '.png'
+    url.png(png_file, scale=6)
+
+    # Update image label
+    img = Image.open(png_file)
+    img = img.resize((150, 150))
+    photo = ImageTk.PhotoImage(img)
+
+    label2.config(image=photo)  # Do not comment this line
+    label2.image = photo  # keep a reference!
+
 
 # Creating tkinter window
 win = Tk()
@@ -29,7 +38,12 @@ label = Label(win, text="Enter text to generate QR Code")
 label.pack()
 entry_text = Entry(win)
 entry_text.pack()
+# Image display
+label2 = Label(win, text="Enter QR Code")
+label2.pack()
 button = Button(win, text="Generate", command=generate_qrcode)
 button.pack()
+
+
 
 win.mainloop()
